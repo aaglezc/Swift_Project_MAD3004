027a0329ad8273abdc39d7f25833b9715dd3042c
MacStudent <macstudent@126-iMac06.local>
2019-02-21 20:59:26 -0500

    IDisplay Protocol

---
+++ Swift_Project_MAD3004/Protocols.swift
@@ -0,0 +1,14 @@
+//
+//  Protocols.swift
+//  Swift_Project_MAD3004
+//
+//  Created by MacStudent on 2019-02-21.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+protocol IDisplay
+{
+    func display() -> String
+}
